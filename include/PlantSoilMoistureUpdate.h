744167c1d4a6064bd33be7aed5ec08e913adf391
adamhoof <adam.hofman1@gmail.com>
2022-09-12 22:48:40 +0200

    works boiiiiiiiiii

--- include/PlantSoilMoistureUpdate.h
+++ include/PlantSoilMoistureUpdate.h
@@ -2,9 +2,10 @@
 
 #include <Arduino.h>
 
-struct SoilMoistureUpdate
+struct PlantSoilMoistureUpdate
 {
     const char* plant_name;
     uint16_t value;
     uint16_t critical_value;
+    bool is_critical_value;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,16 @@
 #include <WifiController.h>
 #include "credentials.h"
 #include <PubSubClient.h>
-#include "SoilMoistureUpdate.h"
-#include "PlantData.h"
+#include "PlantSoilMoistureUpdate.h"
 #include <ArduinoJson.h>
+
+#define uS_TO_S_FACTOR 1000000ULL
+#define TIME_TO_SLEEP 2*60*60
 
 void setup()
 {
     Serial.begin(115200);
-    esp_sleep_enable_timer_wakeup(10000000);
+    esp_sleep_enable_timer_wakeup(15000000);
     WifiController wifiController = WifiController();
     PubSubClient mqttClient = PubSubClient();
     WiFiClient wifiClient = WiFiClient();
@@ -21,32 +23,38 @@ void setup()
     mqttClient.setClient(wifiClient);
     mqttClient.connect(hostname);
 
-    SoilMoistureUpdate soil_moisture_updates[2] {
-            SoilMoistureUpdate {.plant_name = "palm_tree_office", .value = analogRead(34), .critical_value = 2000},
-            SoilMoistureUpdate {.plant_name = "leafy_tree_office", .value = analogRead(35), .critical_value = 2000}
+    PlantSoilMoistureUpdate soil_moisture_updates[2] {
+            PlantSoilMoistureUpdate {.plant_name = "palm_tree_office", .value = analogRead(
+                    34), .critical_value = 2000, .is_critical_value = false},
+            PlantSoilMoistureUpdate {.plant_name = "leafy_tree_office", .value = analogRead(
+                    35), .critical_value = 2000, .is_critical_value = false}
     };
 
     char json_buffer[250];
     StaticJsonDocument<250> json_doc;
 
-    for (SoilMoistureUpdate& soil_moisture_update: soil_moisture_updates) {
-        if (soil_moisture_update.value > 2000) {
-            soil_moisture_update.critical_value = true;
-        }
+    for (PlantSoilMoistureUpdate& update: soil_moisture_updates) {
+        json_doc["PlantName"] = update.plant_name;
+        json_doc["Value"] = update.value;
 
-        json_doc["FlowerName"] = soil_moisture_update.plant_name;
-        json_doc["SensorValue"] = soil_moisture_update.value;
-        json_doc["IsCriticalValue"] = soil_moisture_update.critical_value;
+        if (update.value > update.critical_value) {
+            update.is_critical_value = true;
+        }
+        json_doc["IsCriticalValue"] = update.is_critical_value;
+
+        serializeJson(json_doc, json_buffer);
+
+        char final_publish_topic[strlen(publish_topic) + strlen(update.plant_name)];
+        strcpy(final_publish_topic, publish_topic);
+        strcat(final_publish_topic, update.plant_name);
+
+        mqttClient.publish(final_publish_topic, json_buffer, true);
     }
 
-    serializeJson(json_doc, json_buffer);
-    mqttClient.publish(publish_topic, json_buffer);
-
-    memset(json_buffer, 0, sizeof json_buffer);
-
     mqttClient.disconnect();
-
     wifiController.disconnect();
+    delay(100);
+
     esp_deep_sleep_start();
 }
 
